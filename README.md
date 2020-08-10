7ab18769d91690d1ea3639e38e27331606da9368
Arthur Wang <z5257096@ad.unsw.edu.au>
2020-08-10 12:57:28 +1000

    initial setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# fantasy chess
+
+### ideas
+- derivative of chess
+- chess pieces have hp and attack power
+- chess pieces same moves as normal chess
+- different boards
+- multiplayer
+- play against bots
+- 
---
+++ src/TestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private float x = 0;
+    private float y = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("FKFKFKFK");
+        transform.position = new Vector3(x, y);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        x += 1 * Time.deltaTime;
+        transform.position = new Vector3(x, y);
+    }
+}
